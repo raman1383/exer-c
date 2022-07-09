8ab97892887d6693c31064afd939358151db9b7f
raman1383 <raman83shariati@gmail.com>
2022-07-09 17:46:07 +0430

    r

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-/* count characters in input; 1st version */
+
 main()
 {
-    long nc = 0;
-    while (getchar() != EOF)
-        ++nc;
-    printf("%ld\n", nc);
+
+    return 0;
 }
