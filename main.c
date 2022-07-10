690c25d95b428fc6c3a6c3a1c9ad0a7f78ce1cf9
raman1383 <raman83shariati@gmail.com>
2022-07-10 07:44:09 +0430

    r

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-
+void swap(int *px, int *py);
 int main()
 {
+    int x = 11;
+    int *px = &x;
+    int y = 99;
+    int *py = &y;
+
+    swap(py, px);
+    printf("x:%d  ,  y:%d", *px, *py);
+}
+
+void swap(int *px, int *py)
+{
+    int temp;
+    temp = *px;
+    *px = *py;
+    *py = temp;
 }
