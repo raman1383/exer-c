70f0e76bc25e699359678aaf6bce86174f76448b
raman1383 <raman83shariati@gmail.com>
2022-07-10 15:32:04 +0430

    r

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
+float absoluteValue(float x)
+{
+    if (x < 0)
+        x = -x;
+    return (x);
+}
+// Function to compute the square root of a number
+float squareRoot(float x)
+{
+    const float epsilon = .00001;
+    float guess = 1.0;
+    while (absoluteValue(guess * guess - x) >= epsilon)
+        guess = (x / guess + guess) / 2.0;
+    return guess;
+}
 int main(void)
 {
-    int Fibonacci[15], i;
-    Fibonacci[0] = 0; // by definition
-    Fibonacci[1] = 1; // ditto
-    for (i = 2; i < 15; ++i)
-        Fibonacci[i] = Fibonacci[i - 2] + Fibonacci[i - 1];
-    for (i = 0; i < 15; ++i)
-        printf("%i\n", Fibonacci[i]);
+    printf("squareRoot (2.0) = %f\n", squareRoot(2.0));
+    printf("squareRoot (144.0) = %f\n", squareRoot(144.0));
+    printf("squareRoot (17.5) = %f\n", squareRoot(17.5));
     return 0;
 }
