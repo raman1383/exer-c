4b19ab3d06ec1607e63af12e3f15fe179e4f2833
raman1383 <raman83shariati@gmail.com>
2022-07-11 13:55:32 +0430

    r

--- main.c
+++ main.c
@@ -1,24 +1,87 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-float absoluteValue(float x)
+#define MAX 20
+#define INF 999
+
+int mat[MAX][MAX];
+int V;
+
+int dist[MAX];
+
+int q[MAX];
+int qp = 0;
+
+void enqueue(int v) { q[qp++] = v; }
+
+int cf(void *a, void *b)
 {
-    if (x < 0)
-        x = -x;
-    return (x);
+    int *x = (int *)a;
+    int *y = (int *)b;
+    return *y - *x;
 }
-// Function to compute the square root of a number
-float squareRoot(float x)
+
+int dequeue()
 {
-    const float epsilon = .00001;
-    float guess = 1.0;
-    while (absoluteValue(guess * guess - x) >= epsilon)
-        guess = (x / guess + guess) / 2.0;
-    return guess;
+    qsort(q, qp, sizeof(int), cf);
+    return q[--qp];
 }
-int main(void)
+
+int queue_has_something() { return (qp > 0); }
+
+int visited[MAX];
+int vp = 0;
+
+void dijkstra(int s)
 {
-    printf("squareRoot (2.0) = %f\n", squareRoot(2.0));
-    printf("squareRoot (144.0) = %f\n", squareRoot(144.0));
-    printf("squareRoot (17.5) = %f\n", squareRoot(17.5));
+    dist[s] = 0;
+    int i;
+    for (i = 0; i < V; ++i)
+    {
+        if (i != s)
+        {
+            dist[i] = INF;
+        }
+        enqueue(i);
+    }
+    while (queue_has_something())
+    {
+        int u = dequeue();
+        visited[vp++] = u;
+        for (i = 0; i < V; ++i)
+        {
+            if (mat[u][i])
+            {
+                if (dist[i] > dist[u] + mat[u][i])
+                {
+                    dist[i] = dist[u] + mat[u][i];
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    printf("Enter the number of vertices: ");
+    scanf(" %d", &V);
+    printf("Enter the adj matrix: ");
+    int i, j;
+    for (i = 0; i < V; ++i)
+    {
+        for (j = 0; j < V; ++j)
+        {
+            scanf(" %d", &mat[i][j]);
+        }
+    }
+
+    dijkstra(0);
+
+    printf("\nNode\tDist\n");
+    for (i = 0; i < V; ++i)
+    {
+        printf("%d\t%d\n", i, dist[i]);
+    }
+
     return 0;
 }
