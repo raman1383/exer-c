7bf30939eb418e8a7e6200e83077db5f62aaf289
raman1383 <raman83shariati@gmail.com>
2022-07-13 20:38:29 +0430

    r

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #define merge(a, b) a##b
+#define get(a) #a
 
 int main()
 {
-    printf("%d", merge(12, 34)); // 1234
+    printf("%d", merge(12, 34));  // 1234
+    printf("%s", get(GeeksQuiz)); // GeeksQuiz
     return 0;
 }
