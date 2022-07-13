2c94ba10510d7334bdca983464261b28b9ae0dc6
raman1383 <raman83shariati@gmail.com>
2022-07-13 20:48:02 +0430

    r

--- main.c
+++ main.c
@@ -8,14 +8,30 @@
         i++;                             \
     }
 #define square(x) (x * x)
+static inline int squareV2(int x) { return x * x; }
 
 int main()
 {
-    printf("%d \n", merge(12, 34));  // 1234
+    printf("%d \n", merge(12, 34)); // 1234
+
     printf("%s \n", get(GeeksQuiz)); // GeeksQuiz
+
     int i = 0;
     PRINT(i, 3);
+
     int x = 36 / square(6);
     printf("%d", x);
+
+    int x2 = 36 / squareV2(6);
+    printf("%d", x2);
+
+#if VERBOSE >= 2
+    printf("Trace Message");
+#endif
+
+    printf("Current File :%s\n", __FILE__);
+    printf("Current Date :%s\n", __DATE__);
+    printf("Current Time :%s\n", __TIME__);
+    printf("Line Number :%d\n", __LINE__);
     return 0;
 }
