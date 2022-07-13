645f5bbcce25c9aa2e7d8e43c17e3a50e04edce3
raman1383 <raman83shariati@gmail.com>
2022-07-13 20:42:06 +0430

    r

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #define merge(a, b) a##b
 #define get(a) #a
+#define PRINT(i, limit)                  \
+    while (i < limit)                    \
+    {                                    \
+        printf("%d -> GeeksQuiz \n", i); \
+        i++;                             \
+    }
 
 int main()
 {
-    printf("%d", merge(12, 34));  // 1234
-    printf("%s", get(GeeksQuiz)); // GeeksQuiz
+    printf("%d \n", merge(12, 34));  // 1234
+    printf("%s \n", get(GeeksQuiz)); // GeeksQuiz
+    int i = 0;
+    PRINT(i, 3);
     return 0;
 }
